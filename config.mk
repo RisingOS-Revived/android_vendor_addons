58c8937e0d36ce091c1041085d6a4d134b5d8ac7
Ghosuto <clash.raja10@gmail.com>
2025-08-01 12:22:17 +0000

    Add AOD wallpaper overlay
    Change-Id: I438d982515c5d8663e94ecf27c3ae61e2241b6f6

--- config.mk
+++ config.mk
@@ -578,6 +578,10 @@ PRODUCT_PACKAGES += \
     Bcr
 endif
 
+# Aod wallpaper
+PRODUCT_PACKAGES += \
+    AODWALL
+
 # Include {Lato,Rubik} fonts
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
