0f986c9718213b45e62fdd6a8ba326e52959dd1e
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-02-25 09:41:41 +0800

    addons: ax_tflite: Adding axion tflite common deps
    Change-Id: I679031ee28814d05cd0a1abd4e560905547c38e7
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

---
+++ sdk/ax_tflite/common.mk
@@ -0,0 +1,9 @@
+PRODUCT_PACKAGES += \
+    libtensorflowlite_jni \
+    libtensorflowlite_gpu_jni
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/lib/libtensorflowlite_jni.so \
+    system/lib64/libtensorflowlite_jni.so \
+    system/lib/libtensorflowlite_gpu_jni.so \
+    system/lib64/libtensorflowlite_gpu_jni.so
