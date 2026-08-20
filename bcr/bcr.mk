79cf18a9cda1c1d46e8b5a282f7490986abfedf7
Arman-ATI <altafiarman5@gmail.com>
2026-08-21 01:42:44 +0330

    addons: Update bcr structure

---
+++ bcr/bcr.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+# Include prebuilts packages
+include vendor/addons/bcr/prebuilts/prebuilts.mk
+RELAX_USES_LIBRARY_CHECK=true
+
+# Soong Namespace
+PRODUCT_SOONG_NAMESPACES += vendor/bcr
